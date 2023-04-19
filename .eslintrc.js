dec8d266e126e35b67102fad11fec7e073432bf8
white1golf <changsan2438@gmail.com>
2023-04-19 17:41:29 +0900

    turbo vite react ts tailwind 설정 완료

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   // This tells ESLint to load the config from the package `eslint-config-custom`
   extends: ["custom"],
   settings: {
-    next: {
+    next: { // FIXME: 기존 next 지운거에서 vite react로 변경하도록
       rootDir: ["apps/*/"],
     },
   },
